7394af2f3ca412035cf646c7a7ef2e579bf015c4
Panu Leppäniemi <me@panuleppaniemi.com>
2012-06-11 19:28:45 +0300

    Added Silex.

---
+++ src/app.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+
+$app->get('/', function() use ($app) {
+    //return var_dump($app['twig']);
+    return $app['twig']->render('index.html.twig');
+});
+
+$app->run();
